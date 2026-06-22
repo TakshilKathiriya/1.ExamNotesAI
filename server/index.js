b51cf92682cc5b85051ffe6aa194ee1848238925
TakshilKathiriya <takshilkathiriya2@gmail.com>
2026-06-22 23:10:56 +0530

    updated origin URL of client in server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://one-examnotesaiclient-j2tt.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
